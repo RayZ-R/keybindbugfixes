37c85f1d73bec19bb6f5dbecb9c651b38997ae37
RayZ R <189466013+RayZ-R@users.noreply.github.com>
2025-03-29 21:22:21 +0200

    Fix control sticky key reset for the narrator keybind

--- src/main/java/keybindbugfixes/KeybindBugFixes.java
+++ src/main/java/keybindbugfixes/KeybindBugFixes.java
@@ -2,10 +2,14 @@ package keybindbugfixes;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import keybindbugfixes.config.Config;
 import keybindbugfixes.config.ConfigManager;
+import keybindbugfixes.mixin.KeyBindingAccessor;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.StickyKeyBinding;
 import org.apache.commons.compress.utils.Sets;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +28,16 @@ public class KeybindBugFixes implements ClientModInitializer {
 	static {
 		GsonBuilder builder = new GsonBuilder();
 		GSON = builder.setPrettyPrinting().create();
+	}
+
+	public static void toggleLeftControlStickyKeys() {
+		if (Config.BugFixes.FIX_CONTROL_STICKY_KEY_RESET && Screen.hasControlDown()) {
+			for (StickyKeyBinding stickyKeyBinding : STICKY_KEY_BINDINGS) {
+				if (((KeyBindingAccessor) stickyKeyBinding).getBoundKey().getCode() == GLFW.GLFW_KEY_LEFT_CONTROL) {
+					stickyKeyBinding.setPressed(true);
+				}
+			}
+		}
 	}
 
 	@Override
---
+++ src/main/java/keybindbugfixes/mixin/fix_control_sticky_key_reset/KeyboardMixin.java
@@ -0,0 +1,23 @@
+package keybindbugfixes.mixin.fix_control_sticky_key_reset;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import keybindbugfixes.KeybindBugFixes;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public abstract class KeyboardMixin {
+    @Inject(method = "onKey",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/option/GameOptions;write()V",
+                    shift = At.Shift.AFTER))
+    private void preventControlStickyKeysResetOnNarratorHotkey(CallbackInfo callbackInfo, @Local Screen screen) {
+        if (screen == null) {
+            KeybindBugFixes.toggleLeftControlStickyKeys();
+        }
+    }
+}
