64d71e5ae7f880cc8d07ab34d81d39cefbaf0f1d
RayZ R <189466013+RayZ-R@users.noreply.github.com>
2025-03-31 23:03:04 +0300

    Fix sprint state after using control sticky key reset fix

--- src/main/java/keybindbugfixes/KeybindBugFixes.java
+++ src/main/java/keybindbugfixes/KeybindBugFixes.java
@@ -7,15 +7,13 @@ import keybindbugfixes.config.Config;
 import keybindbugfixes.config.ConfigManager;
 import keybindbugfixes.mixin.KeyBindingAccessor;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.StickyKeyBinding;
-import org.apache.commons.compress.utils.Sets;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.Set;
 
 public class KeybindBugFixes implements ClientModInitializer {
 	public static final String MOD_NAME = "KeybindBugFixes";
@@ -23,6 +21,7 @@ public class KeybindBugFixes implements ClientModInitializer {
 
 	public static final Map<StickyKeyBinding, Boolean> stickyKeyBindingRevertMap = Maps.newHashMap();
 	public static boolean draggingPickKey = false;
+	public static MinecraftClient client;
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 	public static final Gson GSON;
@@ -35,13 +34,21 @@ public class KeybindBugFixes implements ClientModInitializer {
 	public static void revertStickyKeyBindings() {
 		if (Config.BugFixes.FIX_CONTROL_STICKY_KEY_RESET) {
 			for (Map.Entry<StickyKeyBinding, Boolean> entry : stickyKeyBindingRevertMap.entrySet()) {
-				((KeyBindingAccessor) entry.getKey()).setPressedState(entry.getValue());
+				KeyBinding keyBinding = entry.getKey();
+				boolean resetValue = entry.getValue();
+
+				((KeyBindingAccessor) keyBinding).setPressedState(resetValue);
+
+				if (keyBinding.equals(client.options.sprintKey) && !resetValue) {
+					client.player.setSprinting(false);
+				}
 			}
 		}
 	}
 
 	@Override
 	public void onInitializeClient() {
-		 ConfigManager.init();
+		client = MinecraftClient.getInstance();
+		ConfigManager.init();
 	}
 }
