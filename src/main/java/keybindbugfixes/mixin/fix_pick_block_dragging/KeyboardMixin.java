610b5fce9db7106f593acbf1e28a96811b4f349f
RayZ R <189466013+RayZ-R@users.noreply.github.com>
2025-04-04 21:39:58 +0300

    Fix space indent formatting inconsistency

--- src/main/java/keybindbugfixes/mixin/fix_pick_block_dragging/KeyboardMixin.java
+++ src/main/java/keybindbugfixes/mixin/fix_pick_block_dragging/KeyboardMixin.java
@@ -18,83 +18,83 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Keyboard.class)
 public abstract class KeyboardMixin {
-	@Redirect(method = "onKey",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;keyPressed(III)Z"))
-	private static boolean startPickKeyDragging(Screen screen, int keyCode, int scanCode, int modifiers, @Local(ordinal = 2) int action) {
-		boolean processed = screen.keyPressed(keyCode, scanCode, modifiers);
+    @Redirect(method = "onKey",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;keyPressed(III)Z"))
+    private static boolean startPickKeyDragging(Screen screen, int keyCode, int scanCode, int modifiers, @Local(ordinal = 2) int action) {
+        boolean processed = screen.keyPressed(keyCode, scanCode, modifiers);
 
-		if (screen instanceof HandledScreen<?> handledScreen && Config.BugFixes.FIX_PICK_BLOCK_DRAGGING && !processed) {
-			MinecraftClient client = MinecraftClient.getInstance();
-			boolean inCreative = client.interactionManager.hasCreativeInventory();
+        if (screen instanceof HandledScreen<?> handledScreen && Config.BugFixes.FIX_PICK_BLOCK_DRAGGING && !processed) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            boolean inCreative = client.interactionManager.hasCreativeInventory();
 
-			if (client.options.pickItemKey.matchesKey(keyCode, scanCode) && action == GLFW.GLFW_PRESS && inCreative) {
-				HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
+            if (client.options.pickItemKey.matchesKey(keyCode, scanCode) && action == GLFW.GLFW_PRESS && inCreative) {
+                HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
 
-				screenAccessor.setCancelNextRelease(false);
+                screenAccessor.setCancelNextRelease(false);
 
-				if (!screenAccessor.isCursorDragging()) {
-					if (handledScreen.getScreenHandler().getCursorStack().isEmpty()) {
-						Slot focusedSlot = screenAccessor.getFocusedSlot();
-						if (focusedSlot != null && focusedSlot.hasStack()) {
-							screenAccessor.invokeOnMouseClick(focusedSlot, focusedSlot.id, 0, SlotActionType.CLONE);
-						}
-					} else {
-						screenAccessor.setCursorDragging(true);
-						screenAccessor.setHeldButtonCode(-1);
-						screenAccessor.getCursorDragSlots().clear();
-						screenAccessor.setHeldButtonType(2);
-						KeybindBugFixes.draggingPickKey = true;
-					}
-				}
-			}
+                if (!screenAccessor.isCursorDragging()) {
+                    if (handledScreen.getScreenHandler().getCursorStack().isEmpty()) {
+                        Slot focusedSlot = screenAccessor.getFocusedSlot();
+                        if (focusedSlot != null && focusedSlot.hasStack()) {
+                            screenAccessor.invokeOnMouseClick(focusedSlot, focusedSlot.id, 0, SlotActionType.CLONE);
+                        }
+                    } else {
+                        screenAccessor.setCursorDragging(true);
+                        screenAccessor.setHeldButtonCode(-1);
+                        screenAccessor.getCursorDragSlots().clear();
+                        screenAccessor.setHeldButtonType(2);
+                        KeybindBugFixes.draggingPickKey = true;
+                    }
+                }
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		return processed;
-	}
+        return processed;
+    }
 
-	@Redirect(method = "onKey",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;keyReleased(III)Z"))
-	private static boolean stopPickKeyDragging(Screen screen, int keyCode, int scanCode, int modifiers) {
-		boolean processed = screen.keyReleased(keyCode, scanCode, modifiers);
+    @Redirect(method = "onKey",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;keyReleased(III)Z"))
+    private static boolean stopPickKeyDragging(Screen screen, int keyCode, int scanCode, int modifiers) {
+        boolean processed = screen.keyReleased(keyCode, scanCode, modifiers);
 
-		if (screen instanceof HandledScreen<?> handledScreen && Config.BugFixes.FIX_PICK_BLOCK_DRAGGING) {
-			MinecraftClient client = MinecraftClient.getInstance();
+        if (screen instanceof HandledScreen<?> handledScreen && Config.BugFixes.FIX_PICK_BLOCK_DRAGGING) {
+            MinecraftClient client = MinecraftClient.getInstance();
 
-			if (client.options.pickItemKey.matchesKey(keyCode, scanCode)) {
-				HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
+            if (client.options.pickItemKey.matchesKey(keyCode, scanCode)) {
+                HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
 
-				KeybindBugFixes.draggingPickKey = false;
+                KeybindBugFixes.draggingPickKey = false;
 
-				if (screenAccessor.isCursorDragging() && screenAccessor.getHeldButtonCode() != -1) {
-					screenAccessor.setCursorDragging(false);
-					screenAccessor.getCursorDragSlots().clear();
-					screenAccessor.setCancelNextRelease(true);
-					return true;
-				}
+                if (screenAccessor.isCursorDragging() && screenAccessor.getHeldButtonCode() != -1) {
+                    screenAccessor.setCursorDragging(false);
+                    screenAccessor.getCursorDragSlots().clear();
+                    screenAccessor.setCancelNextRelease(true);
+                    return true;
+                }
 
-				if (screenAccessor.getCancelNextRelease()) {
-					screenAccessor.setCancelNextRelease(false);
-					return true;
-				}
+                if (screenAccessor.getCancelNextRelease()) {
+                    screenAccessor.setCancelNextRelease(false);
+                    return true;
+                }
 
-				if (screenAccessor.isCursorDragging() && !screenAccessor.getCursorDragSlots().isEmpty()) {
-					int heldButtonType = screenAccessor.getHeldButtonType();
-					screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(0, heldButtonType), SlotActionType.QUICK_CRAFT);
+                if (screenAccessor.isCursorDragging() && !screenAccessor.getCursorDragSlots().isEmpty()) {
+                    int heldButtonType = screenAccessor.getHeldButtonType();
+                    screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(0, heldButtonType), SlotActionType.QUICK_CRAFT);
 
-					for (Slot slot2x : screenAccessor.getCursorDragSlots()) {
-						screenAccessor.invokeOnMouseClick(slot2x, slot2x.id, ScreenHandler.packQuickCraftData(1, heldButtonType), SlotActionType.QUICK_CRAFT);
-					}
+                    for (Slot slot2x : screenAccessor.getCursorDragSlots()) {
+                        screenAccessor.invokeOnMouseClick(slot2x, slot2x.id, ScreenHandler.packQuickCraftData(1, heldButtonType), SlotActionType.QUICK_CRAFT);
+                    }
 
-					screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(2, heldButtonType), SlotActionType.QUICK_CRAFT);
-				}
+                    screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(2, heldButtonType), SlotActionType.QUICK_CRAFT);
+                }
 
-				screenAccessor.setCursorDragging(false);
-				return true;
-			}
-		}
+                screenAccessor.setCursorDragging(false);
+                return true;
+            }
+        }
 
-		return processed;
-	}
+        return processed;
+    }
 }
--- src/main/java/keybindbugfixes/mixin/rebind_debug_keys/GameModeSelectionScreenMixin.java
+++ src/main/java/keybindbugfixes/mixin/rebind_debug_keys/GameModeSelectionScreenMixin.java
@@ -9,13 +9,13 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(GameModeSelectionScreen.class)
 public abstract class GameModeSelectionScreenMixin {
-	@ModifyConstant(method = "checkForClose", constant = @Constant(intValue = GLFW.GLFW_KEY_F3))
-	private int remapCloseKey(int value) {
-		return Config.Keybinds.DEBUG.getCode();
-	}
+    @ModifyConstant(method = "checkForClose", constant = @Constant(intValue = GLFW.GLFW_KEY_F3))
+    private int remapCloseKey(int value) {
+        return Config.Keybinds.DEBUG.getCode();
+    }
 
-	@ModifyConstant(method = "keyPressed", constant = @Constant(intValue = GLFW.GLFW_KEY_F4))
-	private int remapCycleKey(int value) {
-		return Config.Keybinds.GAME_MODE_CYCLE.getCode();
-	}
+    @ModifyConstant(method = "keyPressed", constant = @Constant(intValue = GLFW.GLFW_KEY_F4))
+    private int remapCycleKey(int value) {
+        return Config.Keybinds.GAME_MODE_CYCLE.getCode();
+    }
 }
