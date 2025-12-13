f1542d14df55b3a07552975b24fea90ae30b36a7
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2025-12-13 21:17:29 +0200

    Backport toggle debug screen when rebinding f3 fix

--- src/main/java/keybindbugfixes/config/Config.java
+++ src/main/java/keybindbugfixes/config/Config.java
@@ -21,6 +21,9 @@ public class Config {
 
         @BugInfo
         public static boolean FIX_MODIFIER_STICKY_KEY = true;
+
+        @BugInfo
+        public static boolean FIX_REBIND_TO_F3 = true;
     }
 
     @CategoryInfo(entryName = "tweak")
