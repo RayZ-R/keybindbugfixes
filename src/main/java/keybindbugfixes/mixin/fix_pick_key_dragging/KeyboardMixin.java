50ba1fae9985a60108a22207ca99fb72ad6dff55
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2026-04-11 19:21:29 +0300

    Refactor pick key dragging mixin

--- src/main/java/keybindbugfixes/mixin/fix_pick_key_dragging/KeyboardMixin.java
+++ src/main/java/keybindbugfixes/mixin/fix_pick_key_dragging/KeyboardMixin.java
@@ -7,7 +7,6 @@ import keybindbugfixes.KeybindBugFixes;
 import keybindbugfixes.config.Config;
 import keybindbugfixes.mixin.HandledScreenAccessor;
 import net.minecraft.client.Keyboard;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.ScreenHandler;
@@ -30,35 +29,32 @@ public abstract class KeyboardMixin {
                                                 Operation<Boolean> original, @Local(ordinal = 2) int action) {
         boolean processed = original.call(screen, keycode, scancode, modifiers);
 
-        if (screen instanceof HandledScreen<?> handledScreen && Config.FIX_PICK_KEY_DRAGGING.value && !processed) {
-            MinecraftClient client = MinecraftClient.getInstance();
-            boolean inCreative = client.player.isInCreativeMode();
+        if (screen instanceof HandledScreen<?> handledScreen
+                && Config.FIX_PICK_KEY_DRAGGING.value
+                && action == GLFW.GLFW_PRESS
+                && !processed) {
+            HandledScreenAccessor accessor = (HandledScreenAccessor) handledScreen;
+            Slot slot = accessor.getFocusedSlot();
+            accessor.setCancelNextRelease(false);
 
-            if (client.options.pickItemKey.matchesKey(keycode, scancode) && action == GLFW.GLFW_PRESS && inCreative) {
-                HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
-
-                screenAccessor.setCancelNextRelease(false);
-
-                if (!screenAccessor.isCursorDragging()) {
-                    if (handledScreen.getScreenHandler().getCursorStack().isEmpty()) {
-                        Slot focusedSlot = screenAccessor.getFocusedSlot();
-                        if (focusedSlot != null && focusedSlot.hasStack()) {
-                            screenAccessor.invokeOnMouseClick(focusedSlot, focusedSlot.id, 0, SlotActionType.CLONE);
-                        }
-                    } else {
-                        screenAccessor.setCursorDragging(true);
-                        screenAccessor.setHeldButtonCode(-1);
-                        screenAccessor.getCursorDragSlots().clear();
-                        screenAccessor.setHeldButtonType(2);
-                        KeybindBugFixes.draggingPickKey = true;
-                    }
+            if (KeybindBugFixes.client.options.pickItemKey.matchesKey(keycode, scancode)
+                    && KeybindBugFixes.client.player.isInCreativeMode()
+                    && slot != null && slot.id != -1
+                    && !accessor.isCursorDragging()) {
+                if (handledScreen.getScreenHandler().getCursorStack().isEmpty()) {
+                    accessor.invokeOnMouseClick(slot, slot.id, 0, SlotActionType.CLONE);
+                    accessor.setCancelNextRelease(true);
+                } else {
+                    accessor.setCursorDragging(true);
+                    accessor.setHeldButtonCode(-1);
+                    accessor.getCursorDragSlots().clear();
+                    accessor.setHeldButtonType(2);
+                    KeybindBugFixes.draggingPickKey = true;
                 }
             }
-
-            return true;
         }
 
-        return processed;
+        return true;
     }
 
     @WrapOperation(
@@ -72,40 +68,40 @@ public abstract class KeyboardMixin {
                                                Operation<Boolean> original) {
         boolean processed = original.call(screen, keycode, scancode, modifiers);
 
-        if (screen instanceof HandledScreen<?> handledScreen && Config.FIX_PICK_KEY_DRAGGING.value) {
-            MinecraftClient client = MinecraftClient.getInstance();
+        if (screen instanceof HandledScreen<?> handledScreen
+                && Config.FIX_PICK_KEY_DRAGGING.value
+                && !processed) {
+            HandledScreenAccessor accessor = (HandledScreenAccessor) handledScreen;
+            Slot slot = accessor.getFocusedSlot();
 
-            if (client.options.pickItemKey.matchesKey(keycode, scancode)) {
-                HandledScreenAccessor screenAccessor = ((HandledScreenAccessor) handledScreen);
-
-                KeybindBugFixes.draggingPickKey = false;
-
-                if (screenAccessor.isCursorDragging() && screenAccessor.getHeldButtonCode() != -1) {
-                    screenAccessor.setCursorDragging(false);
-                    screenAccessor.getCursorDragSlots().clear();
-                    screenAccessor.setCancelNextRelease(true);
-                    return true;
-                }
-
-                if (screenAccessor.getCancelNextRelease()) {
-                    screenAccessor.setCancelNextRelease(false);
-                    return true;
-                }
-
-                if (screenAccessor.isCursorDragging() && !screenAccessor.getCursorDragSlots().isEmpty()) {
-                    int heldButtonType = screenAccessor.getHeldButtonType();
-                    screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(0, heldButtonType), SlotActionType.QUICK_CRAFT);
-
-                    for (Slot slot2x : screenAccessor.getCursorDragSlots()) {
-                        screenAccessor.invokeOnMouseClick(slot2x, slot2x.id, ScreenHandler.packQuickCraftData(1, heldButtonType), SlotActionType.QUICK_CRAFT);
-                    }
-
-                    screenAccessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(2, heldButtonType), SlotActionType.QUICK_CRAFT);
-                }
-
-                screenAccessor.setCursorDragging(false);
+            if (accessor.isCursorDragging() && accessor.getHeldButtonCode() != -1) {
+                accessor.setCursorDragging(false);
+                accessor.getCursorDragSlots().clear();
+                accessor.setCancelNextRelease(true);
                 return true;
             }
+
+            if (accessor.getCancelNextRelease()) {
+                accessor.setCancelNextRelease(false);
+                return true;
+            }
+
+            if (accessor.isCursorDragging() && !accessor.getCursorDragSlots().isEmpty()) {
+                int heldButtonType = accessor.getHeldButtonType();
+                accessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(0, heldButtonType), SlotActionType.QUICK_CRAFT);
+
+                for (Slot dragSlot : accessor.getCursorDragSlots()) {
+                    accessor.invokeOnMouseClick(dragSlot, dragSlot.id, ScreenHandler.packQuickCraftData(1, heldButtonType), SlotActionType.QUICK_CRAFT);
+                }
+
+                accessor.invokeOnMouseClick(null, -999, ScreenHandler.packQuickCraftData(2, heldButtonType), SlotActionType.QUICK_CRAFT);
+            } else if (!handledScreen.getScreenHandler().getCursorStack().isEmpty()
+                    && KeybindBugFixes.client.options.pickItemKey.matchesKey(keycode, scancode)) {
+                accessor.invokeOnMouseClick(slot, slot.id, 0, SlotActionType.CLONE);
+            }
+
+            accessor.setCursorDragging(false);
+            KeybindBugFixes.draggingPickKey = false;
         }
 
         return processed;
