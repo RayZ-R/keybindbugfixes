cdb09df2ef39985256684882ceb6e25fa26c6e7e
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2026-04-14 22:46:24 +0300

    Clean up mixin code

--- src/main/java/keybindbugfixes/mixin/fix_dismount_toggle_sneak/LocalPlayerMixin.java
+++ src/main/java/keybindbugfixes/mixin/fix_dismount_toggle_sneak/LocalPlayerMixin.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin extends AbstractClientPlayer {
-    @Shadow @Final private Minecraft minecraft;
+    @Shadow @Final protected Minecraft minecraft;
 
     @Unique private boolean keybindbugfixes$overrideShiftingPacket = false;
 
