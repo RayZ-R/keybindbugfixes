5b1a1d22e43b3e51c63e18cde408660d583c8cc2
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2026-04-26 23:49:12 +0300

    Add fix_rebind_to_f3 compatibility with amecs

--- src/main/java/keybindbugfixes/mixin/fix_rebind_to_f3/KeyBindsScreenMixin.java
+++ src/main/java/keybindbugfixes/mixin/fix_rebind_to_f3/KeyBindsScreenMixin.java
@@ -1,13 +1,16 @@
 package keybindbugfixes.mixin.fix_rebind_to_f3;
 
 import keybindbugfixes.config.Config;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Options;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.options.OptionsSubScreen;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,23 +18,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(KeyBindsScreen.class)
 public abstract class KeyBindsScreenMixin extends OptionsSubScreen {
+    @Shadow @Nullable public KeyMapping selectedKey;
+
     @Unique private boolean keybindbugfixes$skipNextKeyRelease = false;
 
     public KeyBindsScreenMixin(Screen lastScreen, Options options, Component title) {
         super(lastScreen, options, title);
     }
 
-    @Inject(
-            method = "keyPressed",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/screens/options/controls/KeyBindsList;" +
-                            "resetMappingAndUpdateButtons()V",
-                    shift = At.Shift.AFTER
-            )
-    )
+    @Inject(method = "keyPressed", at = @At("HEAD"))
     private void skipNextKeyRelease(CallbackInfoReturnable<Boolean> callbackInfo) {
-        if (Config.FIX_REBIND_TO_F3.value) {
+        if (Config.FIX_REBIND_TO_F3.value && this.selectedKey != null) {
             this.keybindbugfixes$skipNextKeyRelease = true;
         }
     }
