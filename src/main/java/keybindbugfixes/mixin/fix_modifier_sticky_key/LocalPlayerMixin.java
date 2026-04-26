db87818c1b99c9a86d950eace4b73eb4b386ad82
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2026-04-26 23:17:09 +0300

    Clean up mixins

---
+++ src/main/java/keybindbugfixes/mixin/fix_modifier_sticky_key/LocalPlayerMixin.java
@@ -0,0 +1,19 @@
+package keybindbugfixes.mixin.fix_modifier_sticky_key;
+
+import keybindbugfixes.ToggleKeyStates;
+import keybindbugfixes.config.Config;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin {
+    @Inject(method = "drop", at = @At("HEAD"))
+    private void revertDropStackModifierKey(boolean all, CallbackInfoReturnable<Boolean> callbackInfo) {
+        if (Config.FIX_MODIFIER_STICKY_KEY.value && all) {
+            ToggleKeyStates.revertDropStackModifier();
+        }
+    }
+}
