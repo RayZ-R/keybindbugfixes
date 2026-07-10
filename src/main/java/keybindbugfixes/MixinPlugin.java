49f806d1c858a65c23f9b0f6f316bd8de163e80c
RayZ-R <189466013+RayZ-R@users.noreply.github.com>
2026-07-10 21:34:22 +0300

    Move compatability mixins to inner packages

--- src/main/java/keybindbugfixes/MixinPlugin.java
+++ src/main/java/keybindbugfixes/MixinPlugin.java
@@ -50,7 +50,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
         }
 
         if (!isAmecsApiLoaded) {
-            DISABLED_MIXINS.add("fix_modifier_sticky_key.KeyMappingMixin");
+            DISABLED_MIXINS.add("fix_modifier_sticky_key.amecsapi");
         }
     }
 
